89ca24cb67e778c4c3dae8f4a4071d1e988f052b
drjaska <drjaska83@gmail.com>
2023-02-23 22:09:11 +0200

    some color changes

--- lua/black.lua
+++ lua/black.lua
@@ -1,26 +1,26 @@
 local black = {
+    black    = '#000000';
+    neardark = '#112630';
     gray1    = '#111111';
     gray2    = '#222222';
     gray3    = '#333333';
     gray4    = '#444444';
+    gray     = '#637777';
     gray7    = '#777777';
-    graya    = '#aaaaaa';
-    white    = '#d6deeb';
+    white    = '#aaaaaa';
     bright   = '#eeeeee';
-    black   = '#000000';
-    neardark = '#112630';
-    purple   = '#c792ea';
+    purple   = '#2d2c5d';
+    purple2  = '#c792ea';
+    blue     = '#82aaff';
+    cyan     = '#d6deeb';
+    cyan2    = '#5ca7e4';
     green    = '#c5e478';
     yellow   = '#ffcb8b';
-    gray     = '#637777';
-    blue     = '#82aaff';
-    yellow2  = '#f78c6c';
+    yellow2  = '#ffcb88';
     yellow3  = '#fbec9f';
     yellow4  = '#f4d554';
-    green2   = '#7fdbca';
     red      = '#ff5874';
-    blue2    = '#5ca7e4';
-    purple2  = '#2d2c5d';
+    red2     = '#f78c6c';
     hoki     = '#5f7e97';
     error_fg = '#EF5350';
     info_fg  = '#64B5F6';
@@ -33,18 +33,18 @@ local black = {
     vim.g.terminal_color_0  = black.black
     vim.g.terminal_color_1  = black.red
     vim.g.terminal_color_2  = black.green
-    vim.g.terminal_color_3  = black.yellow2
+    vim.g.terminal_color_3  = black.red2
     vim.g.terminal_color_4  = black.blue
-    vim.g.terminal_color_5  = black.purple
-    vim.g.terminal_color_6  = black.green2
-    vim.g.terminal_color_7  = black.graya
+    vim.g.terminal_color_5  = black.purple2
+    vim.g.terminal_color_6  = black.yellow2
+    vim.g.terminal_color_7  = black.white
     vim.g.terminal_color_8  = black.gray
     vim.g.terminal_color_9  = black.red
     vim.g.terminal_color_10 = black.green
-    vim.g.terminal_color_11 = black.yellow2
+    vim.g.terminal_color_11 = black.red2
     vim.g.terminal_color_12 = black.blue
-    vim.g.terminal_color_13 = black.purple
-    vim.g.terminal_color_14 = black.green2
+    vim.g.terminal_color_13 = black.purple2
+    vim.g.terminal_color_14 = black.yellow2
     vim.g.terminal_color_15 = black.bright
   end
 
@@ -59,11 +59,11 @@ local black = {
 
   function black.load_syntax()
     local syntax = {
-      Normal                               = {fg=black.white,bg=black.black};
-      Terminal                             = {fg = black.white,bg=black.darkblue};
+      Normal                               = {fg=black.yellow,bg=black.black};
+      Terminal                             = {fg = black.yellow,bg=black.darkblue};
       LineNr                               = {fg=black.gray4,   bg=black.black};
       CursorLine                           = {bg=black.neardark};
-      CursorLineNr                         = {fg=black.graya,   bg=black.neardark};
+      CursorLineNr                         = {fg=black.white,   bg=black.neardark};
       ColorColumn                          = {bg=black.gray2};
       Directory                            = {fg=black.blue};
       DiffAdd                              = {fg=black.black,  bg=black.green};
@@ -78,103 +78,103 @@ local black = {
       SignColumn                           = {fg=black.none,    bg=black.black};
       IncSearch                            = {fg=black.bright,  bg=black.yellow};
       NonText                              = {fg=black.gray4};
-      PMenu                                = {bg=black.purple2};
-      PMenuSel                             = {bg=black.purple};
+      PMenu                                = {bg=black.purple};
+      PMenuSel                             = {bg=black.purple2};
       Search                               = {fg=black.black,  bg=black.yellow};
       SpecialKey                           = {fg=black.yellow};
       StatusLine                           = {fg=black.bright,  bg=black.neardark};
       StatusLineNC                         = {fg=black.gray7,   bg=black.neardark};
       Title                                = {fg=black.blue};
-      Visual                               = {fg=black.white,   bg=black.purple2};
+      Visual                               = {fg=black.cyan,   bg=black.purple};
       EndOfBuffer                          = {fg=black.gray4,   bg=black.black };
       Comment                              = {fg=black.gray,    style='italic'};
       Constant                             = {fg=black.green};
       String                               = {fg=black.yellow};
-      Identifier                           = {fg=black.green2};
-      Statement                            = {fg=black.blue};
-      Operator                             = {fg=black.purple};
+      Identifier                           = {fg=black.yellow2};
+      Statement                            = {fg=black.cyan};
+      Operator                             = {fg=black.purple2};
       Exception                            = {fg=black.green};
-      PreProc                              = {fg=black.purple};
-      Type                                 = {fg=black.green};
-      StorageClass                         = {fg=black.purple};
+      PreProc                              = {fg=black.purple2};
+      Type                                 = {fg=black.red2};
+      StorageClass                         = {fg=black.purple2};
       htmlTag                              = {fg=black.gray,    bg=black.none};
       htmlEndTag                           = {fg=black.gray,    bg=black.none};
       Todo                                 = {fg=black.gray7,   bg=black.yellow};
       jsStorageClass                       = {fg=black.blue};
-      jsOperator                           = {fg=black.purple};
-      jsArrowFunction                      = {fg=black.purple};
+      jsOperator                           = {fg=black.purple2};
+      jsArrowFunction                      = {fg=black.purple2};
       jsString                             = {fg=black.yellow};
       jsComment                            = {fg=black.gray,    style='italic'};
       jsFuncCall                           = {fg=black.blue};
-      jsNumber                             = {fg=black.yellow2};
-      jsSpecial                            = {fg=black.yellow2};
-      jsObjectProp                         = {fg=black.green2};
-      jsOperatorKeyword                    = {fg=black.green2};
+      jsNumber                             = {fg=black.red2};
+      jsSpecial                            = {fg=black.red2};
+      jsObjectProp                         = {fg=black.yellow2};
+      jsOperatorKeyword                    = {fg=black.yellow2};
       jsBooleanFalse                       = {fg=black.red};
       jsBooleanTrue                        = {fg=black.red};
-      jsRegexpString                       = {fg=black.blue2};
-      jsConditional                        = {fg=black.purple};
+      jsRegexpString                       = {fg=black.cyan2};
+      jsConditional                        = {fg=black.purple2};
       jsFunction                           = {fg=black.blue};
-      jsReturn                             = {fg=black.purple};
+      jsReturn                             = {fg=black.purple2};
       jsFuncName                           = {fg=black.blue};
       jsParensError                        = {fg=black.blue};
       jsClassDefinition                    = {fg=black.yellow};
-      jsImport                             = {fg=black.purple,  style='italic'};
-      jsFrom                               = {fg=black.purple,  style='italic'};
-      jsModuleAs                           = {fg=black.purple,  style='italic'};
-      jsExport                             = {fg=black.green2};
-      jsExportDefault                      = {fg=black.green2};
-      jsExtendsKeyword                     = {fg=black.purple,  style='italic'};
+      jsImport                             = {fg=black.purple2,  style='italic'};
+      jsFrom                               = {fg=black.purple2,  style='italic'};
+      jsModuleAs                           = {fg=black.purple2,  style='italic'};
+      jsExport                             = {fg=black.yellow2};
+      jsExportDefault                      = {fg=black.yellow2};
+      jsExtendsKeyword                     = {fg=black.purple2,  style='italic'};
       javaScriptReserved                   = {fg=black.blue};
-      javaScriptConditional                = {fg=black.purple};
+      javaScriptConditional                = {fg=black.purple2};
       javaScriptStringS                    = {fg=black.yellow};
       javaScriptBoolean                    = {fg=black.red};
-      javaScriptBraces                     = {fg=black.white};
+      javaScriptBraces                     = {fg=black.cyan};
       javaScriptLineComment                = {fg=black.gray,    style='italic'};
-      javaScriptSpecial                    = {fg=black.yellow2};
+      javaScriptSpecial                    = {fg=black.red2};
       javaScriptFunction                   = {fg=black.blue};
-      javaScriptStatement                  = {fg=black.purple};
+      javaScriptStatement                  = {fg=black.purple2};
       javaScriptException                  = {fg=black.green};
       scssSelectorName                     = {fg=black.green};
       cssTagName                           = {fg=black.red};
       cssClassName                         = {fg=black.green,   style='italic'};
       cssClassNameDot                      = {fg=black.green,   style='italic'};
-      cssBraces                            = {fg=black.white};
-      cssPositioningProp                   = {fg=black.green2};
-      cssBoxProp                           = {fg=black.green2};
-      cssDimensionProp                     = {fg=black.green2};
-      cssTransitionProp                    = {fg=black.green2};
-      cssTextProp                          = {fg=black.green2};
-      cssFontProp                          = {fg=black.green2};
-      cssBorderProp                        = {fg=black.green2};
-      cssBackgroundProp                    = {fg=black.green2};
-      cssUIProp                            = {fg=black.green2};
+      cssBraces                            = {fg=black.cyan};
+      cssPositioningProp                   = {fg=black.yellow2};
+      cssBoxProp                           = {fg=black.yellow2};
+      cssDimensionProp                     = {fg=black.yellow2};
+      cssTransitionProp                    = {fg=black.yellow2};
+      cssTextProp                          = {fg=black.yellow2};
+      cssFontProp                          = {fg=black.yellow2};
+      cssBorderProp                        = {fg=black.yellow2};
+      cssBackgroundProp                    = {fg=black.yellow2};
+      cssUIProp                            = {fg=black.yellow2};
       cssIEUIProp                          = {fg=black.red};
       scssFunctionName                     = {fg=black.green};
       cssPositioningAttr                   = {fg=black.red};
       cssTableAttr                         = {fg=black.red};
       cssCommonAttr                        = {fg=black.red};
-      cssColorProp                         = {fg=black.green2};
-      cssIncludeKeyword                    = {fg=black.green2};
+      cssColorProp                         = {fg=black.yellow2};
+      cssIncludeKeyword                    = {fg=black.yellow2};
       cssKeyFrameSelector                  = {fg=black.green};
       cssPseudoClassId                     = {fg=black.green,   style='italic'};
       cssBorderAttr                        = {fg=black.red};
-      cssValueLength                       = {fg=black.yellow2};
+      cssValueLength                       = {fg=black.red2};
       cssUnitDecorators                    = {fg=black.yellow3};
       cssIdentifier                        = {fg=black.yellow4, style='italic'};
       markdownHeadingDelimiter             = {fg=black.gray};
       markdownCodeDelimiter                = {fg=black.yellow};
-      markdownCode                         = {fg=black.graya};
-      mkdCodeStart                         = {fg=black.white};
-      mkdCodeEnd                           = {fg=black.white};
-      mkdLinkDef                           = {fg=black.green2};
+      markdownCode                         = {fg=black.white};
+      mkdCodeStart                         = {fg=black.cyan};
+      mkdCodeEnd                           = {fg=black.cyan};
+      mkdLinkDef                           = {fg=black.yellow2};
       mkdCodeDelimiter                     = {fg=black.gray,    bg=black.black};
       htmlH1                               = {fg=black.blue,    style='bold'};
       htmlH2                               = {fg=black.blue,    style='bold'};
       htmlH3                               = {fg=black.blue,    style='bold'};
       htmlH4                               = {fg=black.blue};
       htmlH5                               = {fg=black.blue};
-      htmlBold                             = {fg=black.purple,  bg=black.black  ,style='bold'};
+      htmlBold                             = {fg=black.purple2,  bg=black.black  ,style='bold'};
       shComment                            = {fg=black.gray,    bg=black.black  ,style='italic'};
       NERDTreeDir                          = {fg=black.hoki};
       NERDTreeOpenable                     = {fg=black.red};
@@ -182,7 +182,7 @@ local black = {
       NERDTreeHelp                         = {fg=black.gray4,   style='italic'};
       NERDTreeUp                           = {fg=black.gray};
       NERDTreeDirSlash                     = {fg=black.gray};
-      gitcommitSummary                     = {fg=black.white};
+      gitcommitSummary                     = {fg=black.cyan};
       IndentGuidesOdd                      = {bg=black.gray4};
       IndentGuidesEven                     = {bg=black.gray7};
       GitGutterAdd                         = {fg=black.green};
